183f63298771315164d7bd8162d9b013efe29c60
Fil Tuon <f.tuon887@gmail.com>
2021-04-15 20:53:08 -0300

    .

---
+++ funcoes_Explicacao.py
@@ -0,0 +1,28 @@
+'''
+vamos aprender o que são funções ou rotinas e como utilizar funções em Python. Funções são trechos de código que podem ser
+executados em momentos diferentes de nossos códigos em Python.
+Veja como funciona o comando def em Python e como utilizá-lo com parâmetros simples e múltiplos.
+'''
+def mostreaLinha():
+    print('-=' * 30)
+
+
+mostreaLinha()
+print(' SISTEMA DE ALUNOS ')
+mostreaLinha()
+mostreaLinha()
+print(' CADASTRO DE FUNCIONÁRIOS ')
+mostreaLinha()
+mostreaLinha()
+print(' ERRO NO SISTEMA ')
+mostreaLinha()
+
+def texto(txt):
+    print('-=' * 30)
+    print(txt)
+    print('-=' * 30)
+
+    #Programa Principal
+texto(' CURSO EM VIDEO ')
+texto(' APRENDA PYTHON ')
+texto(' FILIPE TUON ')

07fe406c4b6100580534873db6f615b5c35c627b
Fil Tuon <f.tuon887@gmail.com>
2021-04-16 10:18:38 -0300

    .

--- funcoes_Explicacao.py
+++ funcoes_Explicacao.py
@@ -3,6 +3,7 @@ vamos aprender o que são funções ou rotinas e como utilizar funções em Pyth
 executados em momentos diferentes de nossos códigos em Python.
 Veja como funciona o comando def em Python e como utilizá-lo com parâmetros simples e múltiplos.
 '''
+#=================================================================
 def mostreaLinha():
     print('-=' * 30)
 
@@ -16,7 +17,7 @@ mostreaLinha()
 mostreaLinha()
 print(' ERRO NO SISTEMA ')
 mostreaLinha()
-
+#=================================================================
 def texto(txt):
     print('-=' * 30)
     print(txt)
@@ -26,3 +27,12 @@ def texto(txt):
 texto(' CURSO EM VIDEO ')
 texto(' APRENDA PYTHON ')
 texto(' FILIPE TUON ')
+#=================================================================
+def soma(a, b):
+    s = a + b
+    print(s)
+
+    #Programa principal
+soma(4, 5)
+soma(8, 9)
+soma(2, 1)

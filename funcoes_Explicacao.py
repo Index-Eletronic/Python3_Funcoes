d1973ed7e23582aa4b03e7b441704964f846f9e9
Fil Tuon <f.tuon887@gmail.com>
2021-04-19 10:12:41 -0300

    .

--- funcoes_Explicacao.py
+++ funcoes_Explicacao.py
@@ -34,5 +34,48 @@ def soma(a, b):
 
     #Programa principal
 soma(4, 5)
+# pode fazer também soma(a=4, b=5)
 soma(8, 9)
+# pode fazer também soma(b=4, a=5)
 soma(2, 1)
+
+#==================================================================
+def soma(a, b):
+    print(f'A = {a} e B={b}')
+    s= a + b
+    print(f'A soma de A + B = {s}')
+
+# Programa Principal:
+soma(4, 5)
+#=================== EMPACOTAR PARAMETROS - TUPLAS ============================
+def contador(* num): # * desempacotar para receber vários parametros ( Vou recever parametros, quantos? - Não sei.. se vera ai)
+    print(num) # Neste caso criou uma tupla
+    for i in num: # para cada Valor um num
+        print(i, end=' ' )
+    print('FIM')
+    tam = len(num)
+    print(f'Recebi os valor {num} e são ao todos {tam} numeros')
+contador(2, 1, 7)
+contador(8, 0)
+contador(4, 4, 7, 6, 2)
+#=================== EMPACOTAR PARAMETROS - LISTA ============================
+#EX: Dobras os valores de uma lista com valores.
+def dobra(lst): # A lista é uma variavel composta: Não precisa desempacotar.
+    pos = 0 # começa na posição 0
+    while pos < (len(lst)): # Enquado a posição for menor que o tamanho da lista:
+        lst[pos]*=2 # A Lista na posição atual recebe o dobro.
+        pos +=1
+
+
+valores = (6, 3, 9, 1, 0, 2)
+dobra(valores)
+print(valores)
+#==================================================================
+def soma(* valores):
+    s = 0
+    for num in valores:
+        s += num
+    print(f'Somando os valores {valores} temos {s}')
+
+soma(5, 2)
+soma(2, 9, 4)

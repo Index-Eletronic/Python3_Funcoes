b6374ab5b5ec8ab254c37dcdf4c6c3b8495bc0d4
Fil Tuon <f.tuon887@gmail.com>
2021-04-22 20:13:45 -0300

    .

--- funcoes_teoria.py
+++ funcoes_teoria.py
@@ -22,3 +22,20 @@ def contador(i, f, p):
     print('FIM')
 
 # Parametros Opcionais
+def somar(a=0, b=0, c=0): # Quando NÃO tiver parametro, passa a valer 0.
+    s= a + b + c
+    print(f'A soma vale {s}')
+
+
+somar(3,2,5)
+somar(8.4)
+
+#Escopo de variaveis
+
+def soma(a=0, b=0, c=0):
+    s = a + b + c
+    return s
+
+r1 = soma(3,2,5)
+r2 = soma(1,7)
+r3 = soma(4)

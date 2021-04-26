b0a512956be2e53b283a67ad9722c6f7fa817d07
Fil Tuon <f.tuon887@gmail.com>
2021-04-26 19:17:30 -0300

    .

--- exerc_funcoes_Ficha_Jogador.py
+++ exerc_funcoes_Ficha_Jogador.py
@@ -3,13 +3,13 @@ Faça um programa que tenha uma função chamada ficha(), que receba dois parâm
  O programa deverá ser capaz de mostrar a ficha do jogador, mesmo que algum dado não tenha sido informado corretamente.
 '''
 
-def fecha(jog='<Desconhecido>', gol=0):
+def ficha(jog='<Desconhecido>', gol=0):
     print(f'Jogador {jog} fez {gol} gol(s) no campeonato')
 
 
 
 n = str(input('Nome do Jogado: '))
-g = int(input('Numero de gols'))
+g = str(input('Numero de gols: '))
 if g.isnumeric(): # Se g for numerico
     g = int(g)  # Então g recebe o int de g
 else:

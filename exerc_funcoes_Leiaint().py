3cfe09bf650070f3bc581cae7dfa712b301062b2
Fil Tuon <f.tuon887@gmail.com>
2021-04-26 20:23:34 -0300

    .

---
+++ exerc_funcoes_Leiaint().py
@@ -0,0 +1,23 @@
+'''
+Crie um programa que tenha a função leiaInt(), que vai funcionar de forma semelhante ‘
+a função input() do Python, só que fazendo a validação para aceitar apenas um valor numérico. Ex: n = leiaInt(‘Digite um n: ‘)
+'''
+
+#===================== Validando um valor =====================
+
+def leiaInt(msg):
+    ok = False
+    valor = 0
+    while True:
+        n = str(input(msg))
+        if n.isnumeric():
+            valor = int(n)
+            ok = True
+        else:
+            print('\33[0;31mERRO ! Digite um numero inteiro valido\33[m')
+        if ok:
+            break
+    return valor
+#Programa Principal
+n = leiaInt('Digite um numero:')
+print(f'Você acabou de digitar o número {n}')

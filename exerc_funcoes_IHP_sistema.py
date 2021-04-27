021175e38af88d8bd20b79c0ce91ad27eda946a5
Fil Tuon <f.tuon887@gmail.com>
2021-04-26 21:05:25 -0300

    .

--- exerc_funcoes_IHP_sistema.py
+++ exerc_funcoes_IHP_sistema.py
@@ -3,6 +3,7 @@ Faça um mini-sistema que utilize o Interactive Help do Python. O usuário
 vai digitar o comando e o manual vai aparecer. Quando o usuário digitar a palavra ‘FIM’,
 o programa se encerrará. Importante: use cores.
 '''
+from time import sleep
 
 c = ('\033[m',         # cor 0 = sem cores
      '\033[0;30;41m',  # cor 1 = Vermelho
@@ -10,8 +11,8 @@ c = ('\033[m',         # cor 0 = sem cores
      '\033[0;30;43m',  # cor 3 = Amarelo
      '\033[0;30;44m',  # cor 4 = Azul
      '\033[0;30;45m',  # cor 5 = Roxo
-     '\033[0;30',  # cor 6 = Branco
-    );
+     '\033[7;30m',  # cor 6 = Branco
+    )
 
 
 def ajuda(com):
@@ -19,7 +20,7 @@ def ajuda(com):
     print(c[6], end='')
     help(com)
     print(c[0], end='')
-
+    sleep(2)
 
 def titulo(msg, cor=0):
     tam = len(msg) + 4
@@ -28,7 +29,7 @@ def titulo(msg, cor=0):
     print(f'  {msg}')
     print('~' * tam)
     print(c[0], end='') # Para limpar as cores
-
+    sleep(1)
 #Programa Principal
 comando = ''
 while True:
